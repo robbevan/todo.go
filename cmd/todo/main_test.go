3f1ad3e9b6c0eea61476750520f430fc4e795e4c
Rob Bevan <robbevan@xpt.com>
2019-12-15 10:46:59 +0000

    handle multiple cli options with flag

--- cmd/todo/main_test.go
+++ cmd/todo/main_test.go
@@ -5,7 +5,6 @@ import (
 	"os"
 	"os/exec"
 	"path"
-	"strings"
 	"testing"
 )
 
@@ -37,14 +36,14 @@ func TestTodoCLI(t *testing.T) {
 	cmdPath := path.Join(dir, binName)
 
 	t.Run("AddNewTask", func(t *testing.T) {
-		cmd := exec.Command(cmdPath, strings.Split(task, " ")...)
+		cmd := exec.Command(cmdPath, "-task", task)
 		if err := cmd.Run(); err != nil {
 			t.Fatal(err)
 		}
 	})
 
 	t.Run("ListTasks", func(t *testing.T) {
-		cmd := exec.Command(cmdPath)
+		cmd := exec.Command(cmdPath, "-list")
 		out, err := cmd.CombinedOutput()
 		if err != nil {
 			t.Fatal(err)
