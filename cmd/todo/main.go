781aa5024572d9abcf928ae5c92207f1d8f93bdb
Rob Bevan <robbevan@xpt.com>
2019-12-16 12:20:57 +0000

    display cli tool usage

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -11,6 +11,14 @@ import (
 const todoFileName = ".todo.json"
 
 func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(),
+			"%s tool. Developed for The Pragmatic Bookshelf\n", os.Args[0])
+		fmt.Fprintf(flag.CommandLine.Output(), "Copyright 2020\n")
+		fmt.Fprintln(flag.CommandLine.Output(), "Usage information:")
+		flag.PrintDefaults()
+	}
+
 	task := flag.String("task", "", "Task to be included in the todo list")
 	list := flag.Bool("list", false, "List all tasks")
 	complete := flag.Int("complete", 0, "Item to be completed")
